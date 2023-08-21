960fe4339858569530b3e272124861d2f0cc6116
Demigod <austinjobs2shop@gmail.com>
2023-08-21 14:52:37 -0700

    refactor(server): remove ox_inventory dependency. added esx/qb support directly

--- server/server.lua
+++ server/server.lua
@@ -1,3 +1,15 @@
+local frameWork =
+    GetResourceState("es_extended") == "started" and 'esx'
+    or
+    GetResourceState("qb-core") == "started" and 'qb'
+
+local core =
+    (Bridge.frameWork == "esx" and exports["es_extended"]:getSharedObject())
+    or
+    (Bridge.frameWork == "qb" and exports["qb-core"]:GetCoreObject())
+
+local ox_inventory = exports.ox_inventory
+
 local POOL_SIZE = 100
 
 local function tableConcat(t1, t2)
@@ -13,6 +25,38 @@ local function shuffle(tbl)
         tbl[i], tbl[j] = tbl[j], tbl[i]
     end
     return tbl
+end
+
+local function getPlayer(src)
+    if not core then return end
+
+    if frameWork == "esx" then
+        return core.GetPlayerFromId(src)
+    elseif frameWork == "qb" then
+        return core.Functions.GetPlayer(src)
+    end
+end
+
+local function giveItem(src, item, amount, metadata)
+    if GetResourceState("ox_inventory") == "started" then
+        if ox_inventory:CanCarryItem(src, item, amount, metadata) then
+            return ox_inventory:AddItem(src, item, amount, metadata)
+        else
+            local dropId = ox_inventory:CreateDropFromPlayer(src)
+            ox_inventory:AddItem(dropId, item, amount, metadata)
+        end
+        return
+    end
+
+    local Player = getPlayer(src)
+
+    if not Player then return end
+
+    if frameWork == "esx" then
+            Player.addInventoryItem(item, amount, metadata or {})
+    elseif frameWork == "qb" then
+        Player.Functions.AddItem(item, amount, false, metadata or {})
+    end
 end
 
 local function generateLootPoolFromCase(case)
@@ -79,12 +123,11 @@ RegisterNetEvent('demi_lootbox:getQueuedItem', function()
     if not playerLootQueue[source] then
         print("^1 ==================================================================================================")
         print(('^1 [WARNING]: POSSIBLE CHEATER. Player Source %s triggered lootbox get item event while not in queue')
-        :format(source))
+            :format(source))
         print('^1 =================================================================================================')
         print(('^1 PLAYERS IDENTIFIERS = %s'):format(json.encode(GetPlayerIdentifiers(source), { indent = true })))
         print('^1 =================================================================================================')
         return
     end
-
-    exports.ox_inventory:AddItem(source, playerLootQueue[source].name, playerLootQueue[source].amount)
+    giveItem(source, playerLootQueue[source].name, playerLootQueue[source].amount)
 end)
